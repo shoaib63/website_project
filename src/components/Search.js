7debe318d2914410f78d152dda6b8689bd9e7647
Shoaib Miraki <mirakishoaib@gmail.com>
2022-03-18 09:51:05 +0330

    Add counter to cards and set log to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,29 @@ import React, { Component } from 'react';
 import styles from "./Search.module.css";
 
 class Search extends Component {
+    
+    constructor(){
+        super(); 
+        this.state = {
+            search: "",
+        }
+    }
+
+    changeHandler = (event) => {
+        this.setState({
+            search: event.target.value,
+        })
+    }
     render() {
         return (
             <div className={styles.container}>
                 <p>Search What you want</p>
-                <input placeholder='Search...'/>
+                <div>
+                    <input value={this.state.search} onChange={this.changeHandler} placeholder='Search...'/>
+                    <br></br>
+                    <br></br>
+                    <span>{this.state.search}</span>
+                </div>
             </div>
         );
     }
